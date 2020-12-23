fc1dcd243319f188aa4629887cb643aaf9d1db1a
SiAce <siacespark@gmail.com>
2020-12-22 19:55:23 -0500

    add gui

---
+++ gui/src/lib.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+fn main() {
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
